0836e7706cf2c14def4072fe245a9b35a36f48db
Brixton <sivay2k4@yahoo.com>
2020-11-25 23:29:22 +0000

    service-tier

--- src/main/java/org/dell/kube/pages/IPageRepository.java
+++ src/main/java/org/dell/kube/pages/IPageRepository.java
@@ -4,8 +4,13 @@ import java.util.List;
 
 public interface IPageRepository {
     public Page create(Page page);
+
     public Page read(long id);
+
     public List<Page> list();
+
     public Page update(Page page, long id);
+
     public void delete(long id);
 }
+
